b8c299a2c4bd43dd597d92a44ef90b4d25f469c3
David Hecker <HECKERDW@GMAIL.COM>
2021-07-28 21:08:01 -0700

    added javascript

---
+++ func.js
@@ -0,0 +1,11 @@
+function incrementButton() {
+    let element = document.getElementById('incrementText');
+    let value = element.innerHTML;
+
+    value++;
+
+    console.log(value);
+    document.getElementById('incrementText').innerHTML = value;
+
+
+}
