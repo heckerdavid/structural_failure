f854cecb26862bcb3ce80cb42679d25b5ef38a23
David Hecker <HECKERDW@GMAIL.COM>
2021-07-29 11:54:40 -0700

    added popup plus button

--- func.js
+++ func.js
@@ -1,11 +1,22 @@
 function incrementButton() {
     let element = document.getElementById('incrementText');
     let value = element.innerHTML;
+    let num = parseInt(value);
 
-    value++;
+    num += 1;
 
-    console.log(value);
-    document.getElementById('incrementText').innerHTML = value;
-
-
+    console.log(num);
+    document.getElementById('incrementText').innerHTML = num;
 }
+
+function annoyUser() {
+    input = prompt("do you like pop-ups? (yes/no)")
+    if (input.toLowerCase() == "yes" ) {
+        alert('good, here\'s another')
+    } else if (input.toLowerCase() == "no") {
+        alert("too bad, I need this for a grade =)")
+    } else {
+        alert('invalid input but ok.')
+    }
+}
+annoyUser();
